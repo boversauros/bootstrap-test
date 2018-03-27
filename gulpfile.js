5701ed934c982e855597f4da02a8261ec03750d8
Oriol Bover Vila <bover73@gmail.com>
2018-03-27 18:48:29 +0200

    Gulp part 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,14 @@
 
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
+    del = require('del'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    rev = require('gulp-rev'),
+    cleanCss = require('gulp-clean-css'),
+    flatmap = require('gulp-flatmap'),
+    htmlmin = require('gulp-htmlmin'),
+    imagemin = require('gulp-imagemin'),
     browserSync = require('browser-sync');
 
  
@@ -31,7 +39,43 @@ var gulp = require('gulp'),
       
       });
       
-      // Default task
-      gulp.task('default', ['browser-sync'], function() {
-          gulp.start('sass:watch');
+    // Default task
+    gulp.task('default', ['browser-sync'], function() {
+        gulp.start('sass:watch');
+    });
+
+    // Clean
+    gulp.task('clean', function() {
+        return del(['dist']);
+    });
+
+    gulp.task('copyfonts', function() {
+    gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+    .pipe(gulp.dest('./dist/fonts'));
+    });
+    
+    // Images
+    gulp.task('imagemin', function() {
+        return gulp.src('img/*.{png,jpg,gif}')
+        .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
+        .pipe(gulp.dest('dist/img'));
+    });
+
+    gulp.task('usemin', function() {
+        return gulp.src('./*.html')
+        .pipe(flatmap(function(stream, file){
+            return stream
+              .pipe(usemin({
+                  css: [ rev() ],
+                  html: [ function() { return htmlmin({ collapseWhitespace: true })} ],
+                  js: [ uglify(), rev() ],
+                  inlinejs: [ uglify() ],
+                  inlinecss: [ cleanCss(), 'concat' ]
+              }))
+          }))
+          .pipe(gulp.dest('dist/'));
+      });
+      
+      gulp.task('build',['clean'], function() {
+          gulp.start('copyfonts','imagemin','usemin');
       });
